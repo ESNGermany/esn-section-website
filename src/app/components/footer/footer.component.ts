9178308cdcf4c6183d15a6d48789d9a2ab291b3b
kciy <immernochnichtwach@hab-verschlafen.de>
2020-12-01 16:02:02 +0100

    Add pink mode

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -8,4 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
   constructor() {}
   ngOnInit(): void {}
+  pink(): void {
+    const footer = document.getElementById('foot');
+    const footer2 = document.getElementById('foot2');
+    const footer3 = document.getElementById('foot3');
+    footer.setAttribute('style', 'background-color: #ec008c');
+    footer2.setAttribute('style', 'fill: #ec008c');
+    footer3.setAttribute('style', 'fill: #ec008c');
+  }
+  unpink(): void {
+    const footer = document.getElementById('foot');
+    const footer2 = document.getElementById('foot2');
+    const footer3 = document.getElementById('foot3');
+    footer.setAttribute('style', 'background-color: #3a3a3a');
+    footer2.setAttribute('style', 'fill: #3a3a3a');
+    footer3.setAttribute('style', 'fill: #3a3a3a');
+  }
 }
