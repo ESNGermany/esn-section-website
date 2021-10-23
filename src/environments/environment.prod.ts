a3eeaf876d2c3becec9b7fbbc9c9b37b5aa8c172
Christian <christian@gmeiner.org>
2021-10-23 19:18:34 +0200

    Added environment replace strings for strapi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ export const environment = {
   enablePurge: true,
   STRAPI_SECTION_URL: 'REPLACE_STRAPI_SECTIONS_BASEURL', // e.g. https://sections.esn-germany.de/esnfreiburg-
   STRAPI_SECTION_URL_IMAGE: 'https://sections.esn-germany.de',
+  STRAPI_SECTION_ID: 'REPLACE_STRAPI_USER_ID',
 };
