6c52176f3fac0ccaa6d8b0820be46dab30bbe45d
kciy <immernochnichtwach@hab-verschlafen.de>
2021-10-25 19:34:28 +0200

    pretix debugging 3

--- src/app/components/pretix-calendar/pretix-calendar.component.ts
+++ src/app/components/pretix-calendar/pretix-calendar.component.ts
@@ -8,5 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class PretixCalendarComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loadJsFile('https://pretix.eu/widget/v1.en.js');
+  }
+  public loadJsFile(url) {
+    let node = document.createElement('script');
+    node.src = url;
+    node.type = 'text/javascript';
+    document.getElementsByTagName('head')[0].appendChild(node);
+  }
 }
