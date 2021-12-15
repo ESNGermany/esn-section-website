f40e7e559f005d3c88e3268db64dff31a598b0cf
kciy <immernochnichtwach@hab-verschlafen.de>
2021-12-15 12:01:11 +0100

    add all section IDs to env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,30 @@ export const environment = {
   production: false,
   STRAPI_SECTION_URL: 'https://sections.esn-germany.de/website-', // e.g. https://sections.esn-germany.de/esnfreiburg-
   STRAPI_SECTION_URL_IMAGE: 'https://sections.esn-germany.de',
-  STRAPI_SECTION_ID: 13, // 3 = Cologne
+  STRAPI_SECTION_ID: 4
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+// STRAPI_SECTION_IDs:
+// 1: 
+// 2: 
+// 3: Köln
+// 4: Braunschweig
+// 5: Bayreuth
+// 6: 
+// 7: 
+// 8: Saarbrücken
+// 9: Hannover
+// 10: Dortmund
+// 11: Frankfurt
+// 12: 
+// 13: Berlin
+// 14: 
+// 15: 
+// 16: TU Dresden
+// 17: Halle
+// 18: Darmstadt
+// 19: Konstanz
+// 20: MESA
+// 21: 
+// 22: Hildesheim
