4d7dd6fd7b2d42532cb934ac30dd0f38dd88e236
kciy <immernochnichtwach@hab-verschlafen.de>
2021-12-20 19:51:48 +0100

    remove freiburg

--- src/environments/environment.freiburg.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  enablePurge: true,
-  STRAPI_SECTION_URL: 'https://sections.esn-germany.de/website-', // e.g. https://sections.esn-germany.de/esnfreiburg-
-  STRAPI_SECTION_URL_IMAGE: 'https://sections.esn-germany.de',
-  STRAPI_SECTION_ID: '2', // 2 = Freiburg
-};
