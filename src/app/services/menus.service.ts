371460372e70672d88f5615b38cf788c3ac425c6
kciy <immernochnichtwach@hab-verschlafen.de>
2022-02-03 20:54:33 +0100

    add menus service

---
+++ src/app/services/menus.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, shareReplay, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { MessageService } from './message.service';
+
+export interface MenuItem {
+  id: string;
+  MenuTab1: string;
+  MenuTab2: string;
+  MenuTab3: string;
+  MenuTab4: string;
+}
+
+@Injectable()
+export class MenuService {
+  private url =
+    environment.STRAPI_SECTION_URL +
+    'menus?_created_by=' +
+    environment.STRAPI_SECTION_ID;
+  private dataRequest;
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {
+    this.dataRequest = this.http.get<MenuItem[]>(this.url).pipe(
+      shareReplay(1),
+      tap((_) => this.log('fetched menu')),
+      catchError(this.handleError<MenuItem[]>('fetchMenu'))
+    );
+  }
+
+  fetchMenu(): Observable<MenuItem[]> {
+    return this.dataRequest;
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+  private log(message: string) {
+    this.messageService.add(`MenuService: ${message}`);
+  }
+}
