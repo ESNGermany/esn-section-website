18213e12cfcdb409c25de96cfb58fe6729c575cb
kciy <immernochnichtwach@hab-verschlafen.de>
2022-03-18 14:36:52 +0100

    trying to fix calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -79,6 +79,7 @@ FullCalendarModule.registerPlugins([
     AppRoutingModule,
     HttpClientModule,
     MarkdownModule.forRoot(),
+    FullCalendarModule,
   ],
   providers: [
     FullCalendarModule,
