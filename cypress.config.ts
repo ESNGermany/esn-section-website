b6024b1cb36d4a19d4d43dc23d9ea3cf53537e0e
kciy <immernochnichtwach@hab-verschlafen.de>
2022-07-26 12:55:43 +0200

    add cypress and gh action

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: 'mpm8m6',
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:4200',
   },
 });
