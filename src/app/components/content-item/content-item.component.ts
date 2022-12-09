5b67f643dc663e7441b04684814e0c920dc37543
kciy <immernochnichtwach@hab-verschlafen.de>
2022-12-09 15:34:54 +0100

    add fade in on first two elements

--- src/app/components/content-item/content-item.component.ts
+++ src/app/components/content-item/content-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { map, Observable, shareReplay } from 'rxjs';
 
 import { IContentItem, ContentService } from 'src/app/services/content.service';
@@ -15,7 +16,31 @@ export class ContentItemComponent implements OnInit {
   contentInfo$: Observable<IContentItem[]> | undefined;
   public directusImageLink: string = environment.DIRECTUS_URL_IMAGE;
 
-  constructor(private contentService: ContentService) {}
+  constructor(
+    private contentService: ContentService,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    window.addEventListener('scroll', (event) => {
+      const box0 = this.document.querySelector('.box0');
+      if (box0 != null) {
+        if (window.pageYOffset < box0!.clientHeight) {
+          box0!.classList.add('fade-in-element');
+        } else {
+          box0!.classList.remove('fade-in-element');
+        }
+      }
+    });
+    window.addEventListener('scroll', (event) => {
+      const box1 = this.document.querySelector('.box1');
+      if (box1 != null) {
+        if (window.pageYOffset < box1!.clientHeight) {
+          box1!.classList.add('fade-in-element');
+        } else {
+          box1!.classList.remove('fade-in-element');
+        }
+      }
+    });
+  }
 
   async ngOnInit(): Promise<void> {
     this.setContentInfo();
