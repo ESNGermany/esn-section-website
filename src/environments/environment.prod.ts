c5e65b6aabb740d39f9e7ab23b6d60eeb8959b66
kciy <immernochnichtwach@hab-verschlafen.de>
2023-08-04 16:15:04 +0200

    add directus url to env.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
   STRAPI_SECTION_URL: 'https://sections.esn-germany.de/website-', // e.g. https://sections.esn-germany.de/esnfreiburg-
   STRAPI_SECTION_URL_IMAGE: 'https://sections.esn-germany.de',
   SECTION_NAME: REPLACE_SECTION_NAME,
+  DIRECTUS_URL_ITEMS: 'https://directus.esn-germany.de/items/',
   DIRECTUS_URL_IMAGE: 'https://directus.esn-germany.de/assets/',
   DIRECTUS_URL_FILES: 'https://directus.esn-germany.de/files/',
   DIRECTUS_URL: 'https://directus.esn-germany.de/items/website_',
