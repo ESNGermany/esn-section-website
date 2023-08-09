c774f2cdcf22a3c0cf5fbd178b9c99c96091e8ed
kciy <immernochnichtwach@hab-verschlafen.de>
2023-08-09 20:51:47 +0200

    standalone server module

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 
-import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
-
 @NgModule({
-  imports: [
-    AppModule,
-    ServerModule,
-  ],
-  bootstrap: [AppComponent],
+  imports: [ServerModule],
 })
 export class AppServerModule {}
