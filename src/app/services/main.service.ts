efce37749915ab7dfedc370394d1ad28abd5ae04
WMESN <webmaster@esn-germany.de>
2024-03-11 22:31:01 +0100

    +- Fix

--- src/app/services/main.service.ts
+++ src/app/services/main.service.ts
@@ -18,7 +18,7 @@ export class MainService {
   private mainInformationSubject = new BehaviorSubject<MainItem | undefined>(
     undefined,
   );
-  private url = `${env.DIRECTUS_URL}main_information${env.DIRECTUS_SECTION_FILTER}${env.SECTION_NAME}`;
+  private url = `${env.DIRECTUS_URL}main_information`;
 
   constructor(
     private errorService: ErrorService,
